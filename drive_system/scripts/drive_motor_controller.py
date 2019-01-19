3127fb7989f182b31db6e553f41da017bfd4487b
Chuka Munkh-Achit <chinchuluun1029@gmail.com>
2019-01-19 16:16:30 -0600

    WIP #1: Added get_wheel_ang_vel function

--- drive_system/scripts/drive_motor_controller.py
+++ drive_system/scripts/drive_motor_controller.py
@@ -21,10 +21,29 @@ class DriveController:
         # Initialize subscribers
         self.cmd_sub = rospy.Subscriber('drive_cmd', Twist, self.process_drive_cmd)
 
+        #FIXME: update params
+        self.get_wheel_ang_vel(10, 10, 1)
+
 
     # Callback function for new drive commands
     def process_drive_cmd(self, data):
         pass
+
+    @staticmethod
+    def get_wheel_ang_vel(lin_vel, ang_vel, side):
+
+        CONST_ROBOT_WIDTH = 0.75
+        CONST_WHEEL_DIAMETER = 0.15
+
+        if side != -1 and side != 1:
+            raise ValueError("There is an invalid side value.")
+
+        else:
+            out = 0.5 * CONST_ROBOT_WIDTH * ang_vel
+            out = lin_vel + (side * out)
+            out = out / (CONST_WHEEL_DIAMETER / 2)
+            print(out)
+            return out
 
 
 # Run the node
@@ -38,6 +57,8 @@ if __name__ == '__main__':
 
     # Ready to go
     rospy.loginfo("Drive Motor Controller initialized...")
+
+
 
     # Loop continuously
     rate = rospy.Rate(2)
