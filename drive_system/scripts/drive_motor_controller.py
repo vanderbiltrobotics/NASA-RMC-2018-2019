422989a2eac80709595a7b6e27afdf3113abd822
Jiaqi <jiaqi.li@vanderbilt.edu>
2019-01-20 14:02:46 -0600

    WIP #1: Added publisher for custom DriveMotorSpeeds message type. Publishes speeds for each wheel motor.

--- drive_system/scripts/drive_motor_controller.py
+++ drive_system/scripts/drive_motor_controller.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 # ----------------------------- #
-# DESCRIPTION OF THIS NODE HERE #
+# Subscribes to drive command, Publishes custom motor speeds. #
 # ----------------------------- #
 
 # import required packages
 import rospy
 from geometry_msgs.msg import Twist
 from std_msgs.msg import UInt8
-
+from custom_msgs.msg import DriveMotorSpeeds
 
 class DriveController:
 
@@ -16,35 +16,37 @@ class DriveController:
     def __init__(self):
 
     	# Initialize publishers
-        self.speeds_pub = rospy.Publisher('drive_motor_speeds', UInt8, queue_size=1)
+        self.speeds_pub = rospy.Publisher('drive_motor_speeds', DriveMotorSpeeds, queue_size=1)
 
         # Initialize subscribers
         self.cmd_sub = rospy.Subscriber('drive_cmd', Twist, self.process_drive_cmd)
 
-        #FIXME: update params
-        self.get_wheel_ang_vel(10, 10, 1)
-
-
     # Callback function for new drive commands
     def process_drive_cmd(self, data):
-        pass
-
-    @staticmethod
-    def get_wheel_ang_vel(lin_vel, ang_vel, side):
-
         CONST_ROBOT_WIDTH = 0.75
         CONST_WHEEL_DIAMETER = 0.15
 
-        if side != -1 and side != 1:
-            raise ValueError("There is an invalid side value.")
+        #FIXME: Calculations for motor speed
 
-        else:
-            out = 0.5 * CONST_ROBOT_WIDTH * ang_vel
-            out = lin_vel + (side * out)
-            out = out / (CONST_WHEEL_DIAMETER / 2)
-            print(out)
-            return out
+        speed = 0.5 * CONST_ROBOT_WIDTH * self.cmd_sub.angular
+        speed = self.cmd_sub.linear + speed
+        speed = speed / (CONST_WHEEL_DIAMETER / 2)
+        print(speed)
+        return speed
 
+        #publish the motor speeds
+        motor_speeds = DriveMotorSpeeds()
+
+        #determine which side is positive
+        left_side = -1
+        right_side = 1
+        motor_speeds.front_l = speed * left_side
+        motor_speeds.rear_l = speeds * left_side
+        motor_speeds.front_r = speed * right_side
+        motor_speeds.rear_r = speed * right_side
+
+        self.speeds_pub.Publish(motor_speeds)
+        pass
 
 # Run the node
 if __name__ == '__main__':
@@ -57,8 +59,6 @@ if __name__ == '__main__':
 
     # Ready to go
     rospy.loginfo("Drive Motor Controller initialized...")
-
-
 
     # Loop continuously
     rate = rospy.Rate(2)
