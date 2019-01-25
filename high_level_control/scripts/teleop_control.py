6b38d3b78f91cae3365e3677a45789e97b781aac
Jason Lao <jason.lao@vanderbilt.edu>
2019-01-24 20:11:18 -0600

    WIP #4: Created skeleton node file for teleop control

---
+++ high_level_control/scripts/teleop_control.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# ----------------------------- #
+# DESCRIPTION OF THIS NODE HERE #
+# ----------------------------- #
+
+# import required packages
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
+
+class TeleopController:
+    #stuff
+
+
+if __name__ = '__main__':
+    #stuff
