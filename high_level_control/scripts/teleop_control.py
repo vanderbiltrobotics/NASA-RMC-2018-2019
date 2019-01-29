e9e68a682e50b871ab0a0194da6190c090c80d11
Jacob Gloudemans <jacob.gloudemans@vanderbilt.edu>
2019-01-29 16:54:39 -0600

    WIP #4: Added dependencies to package.xml and CMakeLists
    Added geometry_msgs and sensor_msgs as dependencies
    
    Also fixed a minor bug (== vs =)

--- high_level_control/scripts/teleop_control.py
+++ high_level_control/scripts/teleop_control.py
@@ -29,7 +29,7 @@ class TeleopControl:
         self.drive_pub.publish(twist)
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     
     # Initialize as ROS node
     rospy.init_node('teleop_control')
