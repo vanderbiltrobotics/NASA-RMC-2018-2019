7770294cbacd96ca74ac26ff7a815589f9fbe7e1
alexbarnett12 <alexander.barnett@vanderbilt.edu>
2019-01-29 20:03:01 -0600

    WIP #10: Implemented ArucoEKF and tested using random samples from a Gaussian distribution.

--- slam/ArucoEKF.py
+++ slam/ArucoEKF.py
@@ -8,19 +8,39 @@ import numpy as np
 # For AruCo, our state vector consists of (x,y,theta)
 # Second arugment is dimension of the measurement input Z
 # For AruCo, measurement input is (x,y,theta)
-arucoEKF = ExtendedKalmanFilter(3,3)
+arucoEKF = EKF.ExtendedKalmanFilter(3,3)
+
+'''
+EKF.x initialized to zeros by constructor
+
+EKF.F = I; F = state transition matrix; state does not change w/o control input
+
+
+
+'''
 
 # Define the measurement noise covariance matrix
-#arucoEKF.R = 
+arucoEKF.R = np.eye(3) * .001
 
 # Define the process noise covariance matrix
 #arucoEKF.Q = 
 
-# Define the state transition matrix
-#arucoEKF.F = 
-
 # Define the Jacobian matrix for measurement update
-#H = 
+# Robot movement in each direction is independent of the other two directions
+def HJacobian(x):
+    return np.eye(3) 
 
 # Define callback function to calculate the expected sensor measurement
-#def hx():
+# mean state vector is a distance; ArUco pose estimation algorithm measures distance
+# therefore our h(x) is just an identity matrix
+def hx(x):
+    return x
+
+# test code 
+mean = [3,4,3]
+
+while True:
+    z = np.random.multivariate_normal(mean,arucoEKF.R)
+    arucoEKF.predict_update(z,HJacobian,hx)
+    input()
+    print(arucoEKF.x)
