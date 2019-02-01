6616458d07ddd3955964f8a4538889c997493f1c
alexbarnett12 <alexander.barnett@vanderbilt.edu>
2019-01-31 19:34:01 -0600

    WIP #10 Converted to a class, added 3D pose estimate

--- slam/ArucoEKF.py
+++ slam/ArucoEKF.py
@@ -1,34 +1,19 @@
+#!/usr/bin/env python
+
 # Implements an EKF algorithm from the FilterPy library for the Aruco Marker data input
 
 from filterpy.kalman import EKF
 import numpy as np
 
-# Initialize the Extended Kalman Filter Object
-# First argument is dimension of the state vector X
-# For AruCo, our state vector consists of (x,y,theta)
-# Second arugment is dimension of the measurement input Z
-# For AruCo, measurement input is (x,y,theta)
-arucoEKF = EKF.ExtendedKalmanFilter(3,3)
+# import ros packages
+import rospy
+from geometry_msgs.msg import Twist
 
-'''
-EKF.x initialized to zeros by constructor
-
-EKF.F = I; F = state transition matrix; state does not change w/o control input
-
-
-
-'''
-
-# Define the measurement noise covariance matrix
-arucoEKF.R = np.eye(3) * .001
-
-# Define the process noise covariance matrix
-#arucoEKF.Q = 
 
 # Define the Jacobian matrix for measurement update
 # Robot movement in each direction is independent of the other two directions
 def HJacobian(x):
-    return np.eye(3) 
+    return np.eye(6) 
 
 # Define callback function to calculate the expected sensor measurement
 # mean state vector is a distance; ArUco pose estimation algorithm measures distance
@@ -36,11 +21,32 @@ def HJacobian(x):
 def hx(x):
     return x
 
-# test code 
-mean = [3,4,3]
 
-while True:
-    z = np.random.multivariate_normal(mean,arucoEKF.R)
-    arucoEKF.predict_update(z,HJacobian,hx)
-    input()
-    print(arucoEKF.x)
+class ArucoExtendedKalmanFilter:
+
+    def __init__(self):
+
+        # Initialize the Extended Kalman Filter Object
+        # First argument is dimension of the state vector X
+        # For AruCo, our state vector consists of (x,y,theta)
+        # Second arugment is dimension of the measurement input Z
+        # For AruCo, measurement input is (x,y,theta)
+        self.arucoEKF = EKF.ExtendedKalmanFilter(6,6)
+
+        '''
+        EKF.x initialized to zeros by constructor
+
+        EKF.F = I; F = state transition matrix; state does not change w/o control input
+
+
+
+        '''
+
+        # Define the measurement noise covariance matrix
+        self.arucoEKF.R = np.eye(6) * 1
+
+        # Define the process noise covariance matrix
+        #arucoEKF.Q = 
+
+    def update(self,z):
+        self.arucoEKF.predict_update(z,HJacobian,hx)
