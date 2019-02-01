74d05bd1ff0d0529785c9a087cfeec15e943423d
Jason Lao <jason.lao@vanderbilt.edu>
2019-01-31 19:13:36 -0600

    Added comments clarifying on joy.buttons and joy.axes

--- high_level_control/scripts/teleop_control.py
+++ high_level_control/scripts/teleop_control.py
@@ -1,8 +1,33 @@
 #!/usr/bin/env python
 
-# ----------------------------- #
-# DESCRIPTION OF THIS NODE HERE #
-# ----------------------------- #
+# ----------------------------------------------- #
+# Table of index number of /joy.buttons:
+# Index   Button name on the actual controller
+#
+# 0   A
+# 1   B
+# 2   X
+# 3   Y
+# 4   LB
+# 5   RB
+# 6   back
+# 7   start
+# 8   power
+# 9   Button stick left
+# 10  Button stick right
+
+# Table of index number of /joy.axes:
+# Index   Axis name on the actual controller
+#
+# 0   Moving left joystick left (+) and right (-) changes rotational velocity
+# 1   Moving left joystick up (+) and down (-) changes linear velocity
+# 2   LT
+# 3   Left/Right Axis stick right
+# 4   Up/Down Axis stick right
+# 5   RT
+# 6   cross key left/right
+# 7   cross key up/down
+# ----------------------------------------------- #
 
 # import required packages
 import rospy
