cb9617c7840bc2d3e575daf62fbecc307636ba52
Jacob Gloudemans <jacob.gloudemans@vanderbilt.edu>
2019-02-06 01:04:42 -0600

    WIP #10: Fixed a variety of minor bugs in testEKF.py

--- slam/testEKF.py
+++ slam/testEKF.py
@@ -3,12 +3,12 @@
 # Tests the effectiveness of an EKF on ArUco Board pose data. Subscribes to both raw ArUco pose
 # data and EKF data and compares their standard deviation and covariance.
 
-from filterpy.kalman import EKF
-import numpy as np
-
 # import ros packages
 import rospy
 from geometry_msgs.msg import Pose, PoseWithCovarianceStamped
+
+# Import other required packages
+import numpy as np
 
 
 class ArucoPoseTracker:
@@ -31,47 +31,53 @@ class ArucoPoseTracker:
         self.indexEKF = 0
 
         # Store raw and filtered aruco pose error
-        self.arucoRaw = np.zeros((7,total_data_points))
-        self.arucoEKF = np.zeros((7,total_data_points))
+        self.arucoRaw = np.zeros((7, self.totalDataPoints))
+        self.arucoEKF = np.zeros((7, self.totalDataPoints))
         self.arucoEKFCov = np.zeros((6,6))
 
         # Store mean and standard deviation of all dimensions. 
         # The mean is stored in column 1 and the standard deviation in column 2.
         self.arucoRawMeanAndStd = np.zeros((7,2))
-        self.arucoEKFMeanandStd = np.zeros(7,2))
+        self.arucoEKFMeanAndStd = np.zeros((7,2))
 
     # Record raw aruco error
     def exportRaw(self,pose):
-        
-        # Store raw error in the next open array index
-        self.arucoRaw[0][self.indexRaw] = pose.position.x - self.trueX
-        self.arucoRaw[1][self.indexRaw] = pose.position.y - self.trueY
-        self.arucoRaw[2][self.indexRaw] = pose.position.z - self.trueZ
-        self.arucoRaw[3][self.indexRaw] = pose.orientation.x - self.trueOrientationX
-        self.arucoRaw[4][self.indexRaw] = pose.orientation.y - self.trueOrientationY
-        self.arucoRaw[5][self.indexRaw] = pose.orientation.z - self.trueOrientationZ
-        self.arucoRaw[6][self.indexRaw] = pose.orientation.w - self.trueOrientationW
 
-        # Increment raw index
-        self.indexRaw += 1
+        # Collect up to specified number of data points
+        if self.indexRaw < self.totalDataPoints:
+
+            # Store raw error in the next open array index
+            self.arucoRaw[0][self.indexRaw] = pose.position.x - self.trueX
+            self.arucoRaw[1][self.indexRaw] = pose.position.y - self.trueY
+            self.arucoRaw[2][self.indexRaw] = pose.position.z - self.trueZ
+            self.arucoRaw[3][self.indexRaw] = pose.orientation.x - self.trueOrientationX
+            self.arucoRaw[4][self.indexRaw] = pose.orientation.y - self.trueOrientationY
+            self.arucoRaw[5][self.indexRaw] = pose.orientation.z - self.trueOrientationZ
+            self.arucoRaw[6][self.indexRaw] = pose.orientation.w - self.trueOrientationW
+
+            # Increment raw index
+            self.indexRaw += 1
 
     # Record ekf aruco error
-    def exportFiiltered(self,poseWithCovariance):
+    def exportFiltered(self,poseWithCovariance):
 
-        # Store ekf pose error in the next open array index
-        self.arucoEKF[0][self.indexEKF] = poseWithCovariance.pose.pose.x - self.trueX 
-        self.arucoEKF[1][self.indexEKF] = poseWithCovariance.pose.pose.y - self.trueY 
-        self.arucoEKF[2][self.indexEKF] = poseWithCovariance.pose.pose.z - self.trueZ 
-        self.arucoEKF[3][self.indexEKF] = poseWithCovariance.pose.orientation.x - self.trueOrientationX
-        self.arucoEKF[4][self.indexEKF] = poseWithCovariance.pose.orientation.y - self.trueOrientationY
-        self.arucoEKF[5][self.indexEKF] = poseWithCovariance.pose.orientation.z - self.trueOrientationZ
-        self.arucoEKF[6][self.indexEKF] = poseWithCovariance.pose.orientation.w - self.trueOrientationW
+        # Collect up to specified number of data points
+        if self.indexEKF < self.totalDataPoints:
 
-        # Store ekf covariance data in the next open array index
-        self.arucoEKFCov = poseWithCovariance.pose.covariance
+            # Store ekf pose error in the next open array index
+            self.arucoEKF[0][self.indexEKF] = poseWithCovariance.pose.pose.x - self.trueX
+            self.arucoEKF[1][self.indexEKF] = poseWithCovariance.pose.pose.y - self.trueY
+            self.arucoEKF[2][self.indexEKF] = poseWithCovariance.pose.pose.z - self.trueZ
+            self.arucoEKF[3][self.indexEKF] = poseWithCovariance.pose.orientation.x - self.trueOrientationX
+            self.arucoEKF[4][self.indexEKF] = poseWithCovariance.pose.orientation.y - self.trueOrientationY
+            self.arucoEKF[5][self.indexEKF] = poseWithCovariance.pose.orientation.z - self.trueOrientationZ
+            self.arucoEKF[6][self.indexEKF] = poseWithCovariance.pose.orientation.w - self.trueOrientationW
 
-        # Increment EKF index
-        self.indexEKF += 1
+            # Store ekf covariance data in the next open array index
+            self.arucoEKFCov = poseWithCovariance.pose.covariance
+
+            # Increment EKF index
+            self.indexEKF += 1
 
     # Calculate mean and standard deviation 
     def calculateMeanAndStd(self):
@@ -84,15 +90,16 @@ class ArucoPoseTracker:
             self.arucoEKFMeanAndStd[x][1] = np.std(self.arucoEKF[x])
 
         print(self.arucoRawMeanAndStd)
-        print(self.arucoEKFMeanandStd)
+        print(self.arucoEKFMeanAndStd)
         
 
 # Run ROS Node
-if(__name__ == "__main__"):
+if __name__ == "__main__":
 
     # Init ros node as test_ekf
     rospy.init_node("test_ekf")
 
+    # Read parameters from server
     totalDataPoints = rospy.get_param("total_data_points")
     trueX = rospy.get_param("true_x")
     trueY = rospy.get_param("true_y")
@@ -106,13 +113,15 @@ if(__name__ == "__main__"):
     aruco_pose_tracker = ArucoPoseTracker(trueX,trueY,trueZ,
                         trueOrientationX,trueOrientationY,trueOrientationZ,trueOrientationW,totalDataPoints)
 
-    while aruco_pose_tracker.indexRaw <= total_data_points and
-            aruco_pose_tracker.indexEKF <= total_data_points:
+    # Create subscribers to raw and filtered pose topics
+    poseSubscriber_raw = rospy.Subscriber('aruco_pose_raw', Pose, aruco_pose_tracker.exportRaw)
+    poseSubscriber_filter_cov = rospy.Subscriber('aruco_pose_filtered_covariance', PoseWithCovarianceStamped,
+                                                 aruco_pose_tracker.exportFiltered)
 
-        poseSubscriber_raw = rospy.Subscriber('aruco_pose_raw', Pose, aruco_pose_tracker.exportRaw)
-        poseSubscriber_filter_cov = rospy.Subscriber('aruco_pose_filtered_covariance', PoseWithCovarianceStamped,exportFiltered)
-
-        rospy.spin()
+    # Wait until enough data points have been collected
+    while aruco_pose_tracker.indexRaw <= totalDataPoints and \
+            aruco_pose_tracker.indexEKF <= totalDataPoints:
+        pass
 
     # Calculate mean and standard deviation once done collecting data. Print to console.
     aruco_pose_tracker.calculateMeanAndStd()
