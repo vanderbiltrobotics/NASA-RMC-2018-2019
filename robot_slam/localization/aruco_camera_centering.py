34ba21ccce60a1213fb443773216d53ba029eca1
alexbarnett12 <alexander.barnett@vanderbilt.edu>
2019-03-20 11:52:29 -0500

    WIP 10 Fully cleaned camera centering script

--- robot_slam/localization/aruco_camera_centering.py
+++ robot_slam/localization/aruco_camera_centering.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
-# Receives ROS messages about if an Aruco marker has been detected and the average
-# x location of its corners. Centers the camera using a P feedback loop on this x-coordinate.
+# This program keeps a camera mounted to a servo focused on an AruCo marker by rotating the servo in the 
+# required direction as the AruCo marker position changes. It receives ROS messages about if an Aruco marker has been detected and 
+# the average x location of its corners. Centers the camera using a Proportional gain feedback loop. When there is no AruCo marker 
+# in sight, the servo will sweep from the minimum bound to maximum bound and back until it finds an AruCo marker.
+
+# Alex Barnett, 2019
 
 # Import ROS packages
 import rospy
@@ -16,49 +20,54 @@ import tf.transformations
 import cv2.aruco as aruco
 import numpy as np
 
+# Class with internal state defining required camera dimensions and knowledge about the position of 
+# the Aruco marker being detected.
 class ArucoCamera:
 
     def __init__(self):
-
-        # Initialize an Aruco camera object
-
+        
+        # Camera dimensions
         self.cameraCenterX = rospy.get_param("image_width", default=640) / 2  # Camera center 
-        self.theta = Int32()  # ROS object that stores current servo theta orientation
-        self.error = 0        # Error between current theta and where the camera should be 
-        self.markerCenter = 0 # Stores the center position of the marker
-        self.minTheta = -90   # Minimum servo bound
-        self.maxTheta = 90    # Maximum servo bound
-        self.markerDetected = False # True if an AruCo marker is in view
-        self.sweepDirection = 1 # Controls the direction of sweep
 
-        # Proportional gain; It represents a transformation from the camera pixel subspace to servo angle. 
+        # Servo state
+        self.theta = Int32()         # ROS object that stores current servo theta orientation
+        self.error = 0               # Error between current theta and where the camera should be 
+        self.minTheta = -90          # Minimum servo bound
+        self.maxTheta = 90           # Maximum servo bound
+
+        # Marker paramters
+        self.markerDetected = False  # True if an AruCo marker is in view
+        self.markerCenter = 0        # Stores the center position of the marker
+        
+        # Camera sweep parameters
+        self.sweepDirection = 1      # Controls the direction of sweep
+        self.deltaTheta = 1          # Servo angle change per loop during camera sweep
+
+        # Proportional gain; represents a transformation from the camera pixel subspace to servo angle. 
         # Vary the magnitude of k to change how much the servo angle should change based off the error
         self.k = -0.0005 
 
-        
-        self.deltaTheta = 1
-        self.rangeOfMax = 2
-
+        # Publish servo theta 
         self.servoThetaPub = rospy.Publisher("aruco/servo_theta", Int32, queue_size=0)
 
-    # Decides whether to track Aruco marker or sweep camera
+    # Update if there is an AruCo marker in the camera's FOV
     def updateMarkerDetected(self,detected):
         self.markerDetected = detected.data
 
-    # Update the x position of the Aruco marker
+    # Update the x-coordinate of the pixel of the AruCo marker center. Stores marker center relative
+    # to the center of the camera
     def updateSetPoint(self,markerCenter):
-        self.markerCenter = markerCenter.data
+        self.markerCenter = markerCenter.data - self.cameraCenterX
     
-    # Rotate servo to track the Aruco marker or sweep the camera to find a marker
+    # Rotate servo to track the Aruco marker or sweep the camera until a marker is found
     def updatePosition(self):
         if self.markerDetected:
-            self.error = self.markerCenter - self.cameraCenterX  # servo will track Aruco marker
+            self.error = self.markerCenter  # Find error relative to camera center
             self.k = -0.0005
         else:
-            
-            # Update set point
-            self.error += self.deltaTheta*self.sweepDirection
-            self.k = 1
+            # Will increment servo by deltaTheta in the sweep direction
+            # Divides by P gain so that the P gain will cancel out in new theta calculation
+            self.error += self.deltaTheta*self.sweepDirection / self.k
 
         # Calculate new servo position
         self.theta.data += self.k * self.error
@@ -91,6 +100,7 @@ if __name__ == "__main__":
     # Set loop rate
     r = rospy.Rate(10)  # 10hz
 
+    # Run continuously
     while not rospy.is_shutdown():
         arucoCamera.updatePosition()
         r.sleep()
