26ec41f78ea5f939a1c16556adbbc7db40c60f05
Baibhav Vatsa <baibhavvatsa@gmail.com>
2019-04-25 14:08:57 -0500

    ui improvements

--- robot_rqt_plugin/src/robot_rqt_plugin/my_module.py
+++ robot_rqt_plugin/src/robot_rqt_plugin/my_module.py
@@ -31,21 +31,6 @@ class MyPlugin(Plugin):
         # Create QWidget
         self._widget = QtWidgets.QWidget()
 
-        # Sets up the functions to be called on clicking on buttons
-        self._widget.hard_stop.clicked[bool].connect(self._hardstop)
-        self._widget.soft_stop.clicked[bool].connect(self._softstop)
-        self._widget.control.clicked[bool].connect(self._switchmode)
-
-        # Sets up the variables and the text label
-        self.mode_val = False
-        self.soft_stop = False
-        self.decide_text()
-
-        # Sets the topics and the publishing rate
-        self.control_publisher = rospy.Publisher("control_mode", Bool, queue_size=0)
-        self.stop_publisher = rospy.Publisher("soft_stop", Bool, queue_size=0)
-        self.rate = rospy.Rate(10)
-
         # Get path to UI file which should be in the "resource" folder of this package
         ui_file = os.path.join(rospkg.RosPack().get_path('robot_rqt_plugin'), 'resource', 'MyPlugin.ui')
         # Extend the widget with all attributes and children from UI file
@@ -62,23 +47,42 @@ class MyPlugin(Plugin):
         # Add widget to the user interface
         context.add_widget(self._widget)
 
+        # Sets up the functions to be called on clicking on buttons
+        self._widget.hard_stop.clicked[bool].connect(self._hardstop)
+        self._widget.soft_stop.clicked[bool].connect(self._softstop)
+        self._widget.control.clicked[bool].connect(self._switchmode)
+
+        # Sets up the variables and the text label
+        self.mode_val = False
+        self.soft_stop = False
+        self.decide_text()
+
+        # Sets the topics and the publishing rate
+        self.control_publisher = rospy.Publisher("control_mode", Bool, queue_size=0)
+        self.stop_publisher = rospy.Publisher("soft_stop", Bool, queue_size=0)
+        self.rate = rospy.Rate(10)
+
         # Starts a thread to run in parallel publishing messages
-        threading.Thread(target=self.publish_manually, args=(0,)).start()
+        threading.Thread(target=self.publish_manually).start()
 
     def _hardstop(self):
         # Kills all the running nodes
         os.system("rosnode kill -a")
+        # Updates the text label
+        self._widget.hard.setText('All nodes killed')
 
     def _softstop(self):
         # Changes the value of the soft_stop variable
         # False means not stopped and True means stopped
         self.soft_stop = not self.soft_stop
+        # Updates the text labels
+        self.decide_text()
 
     def _switchmode(self):
         # Changes the value of the mode_val variable
         # False means automatic and True means manual
         self.mode_val = not self.mode_val
-        # Updates the text label
+        # Updates the text labels
         self.decide_text()
 
     def publish_manually(self):
@@ -91,8 +95,9 @@ class MyPlugin(Plugin):
             self.rate.sleep()
 
     def decide_text(self):
-        # Sets the value for the text label
+        # Sets the value for the text labels
         self._widget.mode.setText(['Manual' if self.mode_val else 'Automatic'][0])
+        self._widget.soft.setText(['Enabled' if self.soft_stop else 'Disabled'][0])
 
     # def shutdown_plugin(self):
     #     # unregister all publishers here
