850ab8ca262a97a02a36f6ab0455b55100932d39
Jacob Gloudemans <jacob.gloudemans@vanderbilt.edu>
2019-04-27 12:55:18 -0500

    Fixed line in CMakeLists for rqt_plugin

--- robot_rqt_plugin/src/robot_rqt_plugin/my_module.py
+++ robot_rqt_plugin/src/robot_rqt_plugin/my_module.py
@@ -33,10 +33,13 @@ class MyPlugin(Plugin):
 
         # Get path to UI file which should be in the "resource" folder of this package
         ui_file = os.path.join(rospkg.RosPack().get_path('robot_rqt_plugin'), 'resource', 'MyPlugin.ui')
+
         # Extend the widget with all attributes and children from UI file
         loadUi(ui_file, self._widget)
+
         # Give QObjects reasonable names
         self._widget.setObjectName('MyPluginUi')
+
         # Show _widget.windowTitle on left-top of each plugin (when
         # it's set in _widget). This is useful when you open multiple
         # plugins at once. Also if you open multiple instances of your
