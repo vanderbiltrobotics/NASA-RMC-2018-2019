c0cefe85bc2277983ff51e447690a0cb9405af7f
Jacob Gloudemans <jacob.gloudemans@vanderbilt.edu>
2019-05-04 00:33:45 -0500

    Another bug fix for pose publisher

--- robot_slam/localization/aruco_camera_centering.py
+++ robot_slam/localization/aruco_camera_centering.py
@@ -93,7 +93,11 @@ class ArucoCamera:
         camera_to_mount.position.x = 0
         camera_to_mount.position.y = 0
         camera_to_mount.position.z = 0
-        camera_to_mount.orientation.z = tf.transformations.quaternion_from_euler(0, 0, radians(self.theta.data))[3]
+        quaternion = tf.transformations.quaternion_from_euler(0, 0, radians(self.theta.data))
+        camera_to_mount.orientation.x = quaternion[0]
+        camera_to_mount.orientation.x = quaternion[1]
+        camera_to_mount.orientation.x = quaternion[2]
+        camera_to_mount.orientation.x = quaternion[3]
         self.lensPosePub.publish(camera_to_mount)
 
 
